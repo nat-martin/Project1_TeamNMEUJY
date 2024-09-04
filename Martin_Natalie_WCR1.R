1054ff6391d886a7ea1f1fa0ce796d87856bf0b7
nat-martin <martaaq@bc.edu>
2024-09-04 14:29:25 -0400

    Add files via upload

---
+++ Martin_Natalie_WCR1.R
@@ -0,0 +1,14 @@
+#Martin_Natalie_WRC1.R
+
+#Question 1 
+my_vec <- 1:10
+print(my_vec[6])
+my_vec/2
+mean(my_vec)
+
+#Question 2
+for (x in my_vec) {
+  print(x/2)
+  paste(x/2, "is half of", x)
+  
+}
